999c79b6f16e5f2cbee6d7ccb19956eb9d69f811
rubyfrancesgit <ruby.cuzens@gmail.com>
2022-03-30 15:55:01 +1300

    Front end inputs and js checks complete

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,1 +1,43 @@
-console.log('linked');
+console.log("linked");
+
+$(document).ready(function() {
+    console.log("document ready");
+
+    let url;
+
+    $.ajax({
+        url: "config.json",
+        type: "GET",
+        crossDomain: true,
+        dataType: "json",
+        success: function(configData) {
+            url = `${configData.SERVER_URL}:${configData.SERVER_PORT}`;
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    }); // end of first ajax
+
+    $("#signUpBtn").click(function() {
+        event.preventDefault();
+
+        let username = $("#signUpUsername").val();
+        let email = $("#signUpEmail").val();
+        let password = $("#signUpPassword").val();
+        let seller = document.querySelector('input[name="signUpSeller"]:checked').value;
+        let storeName = $("#signUpStoreName").val();
+        let storeDescription = $("#signUpStoreDescription").val();
+
+        console.log(username, email, password, seller, storeName, storeDescription);
+
+        if (username == "" || email == "" || password == "") {
+            alert("Please enter all fields");
+        } else if (seller == "yes" && username == "" || email == "" || password == "" || storeName == "" || storeDescription == "") {
+            alert("Please enter all fields, including store fields");
+        } else {
+            console.log("Form complete");
+        }
+
+    });
+
+}); // end of document.ready
